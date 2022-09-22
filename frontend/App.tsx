5862068a801a64d6a2deb7ff54575bf66603e162
Eunsoo Shin <me@esinx.net>
2022-09-22 17:05:19 +0900

    setup(frontend): expo, typescript, eslint, prettier

---
+++ frontend/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+	return (
+		<View>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
+}
---
+++ frontend/app.config.ts
@@ -0,0 +1,8 @@
+import { ExpoConfig } from "@expo/config-types"
+
+const config: ExpoConfig = {
+	slug: "tapa",
+	name: "TAPA",
+}
+
+export default config
