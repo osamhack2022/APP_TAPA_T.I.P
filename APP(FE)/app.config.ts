f136379bd90930f0fb9abc7629514f930c5f0601
Eunsoo Shin <62971511+esinx@users.noreply.github.com>
2022-09-23 04:31:23 +0000

    env(frontend): basic react dev configuration

--- APP(FE)/app.config.ts
+++ APP(FE)/app.config.ts
@@ -1,8 +1,33 @@
-import { ExpoConfig } from "@expo/config-types"
+import 'dotenv/config'
+
+import { ExpoConfig } from '@expo/config-types'
 
 const config: ExpoConfig = {
-	slug: "tapa",
-	name: "TAPA",
+	slug: 'tapa',
+	name: 'TAPA',
+	version: '1.0.0',
+	orientation: 'portrait',
+	icon: './assets/icon.png',
+	userInterfaceStyle: 'light',
+	splash: {
+		image: './assets/splash.png',
+		resizeMode: 'contain',
+		backgroundColor: '#ffffff',
+	},
+	ios: {
+		bundleIdentifier: 'com.team-ip.tapa',
+		buildNumber: '1.0.0',
+	},
+	android: {
+		adaptiveIcon: {
+			foregroundImage: './assets/adaptive-icon.png',
+			backgroundColor: '#FFFFFF',
+		},
+		package: 'com.team-ip.tapa',
+	},
+	extra: {
+		backendBaseURL: process.env.BACKEND_BASE_URL ?? 'https://api.tip-tapa.kr',
+	},
 }
 
 export default config
