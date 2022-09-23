763772faa796ddd966c886bccc067243ed3e17fb
Eunsoo Shin <me@esinx.net>
2022-09-23 14:35:47 +0900

    feat(frontend): basic persistence with firebase auth

--- APP(FE)/utils/firebase.ts
+++ APP(FE)/utils/firebase.ts
@@ -1,5 +1,7 @@
+import * as SecureStore from 'expo-secure-store'
 import { FirebaseOptions, initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { initializeAuth, ReactNativeAsyncStorage } from 'firebase/auth'
+import { getReactNativePersistence } from 'firebase/auth/react-native'
 
 const firebaseConfig: FirebaseOptions = {
 	apiKey: '',
@@ -12,8 +14,19 @@ const firebaseConfig: FirebaseOptions = {
 	storageBucket: '',
 }
 
+/**
+ * A wrapper around SecureStore(expo-secure-store) as ReactNativeAsyncStorage
+ */
+const AsyncStorage: ReactNativeAsyncStorage = {
+	getItem: async key => SecureStore.getItemAsync(key),
+	removeItem: async key => SecureStore.deleteItemAsync(key),
+	setItem: async (key, value) => SecureStore.setItemAsync(key, value),
+}
+
 const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
+const auth = initializeAuth(app, {
+	persistence: getReactNativePersistence(AsyncStorage),
+})
 
 const firebase = {
 	app,
