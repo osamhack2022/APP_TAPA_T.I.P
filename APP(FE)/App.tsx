ce39c6f14b68d51e0c74e828373970b9ecb7e590
Eunsoo Shin <me@esinx.net>
2022-10-05 23:36:12 +0900

    fix(font): show splash during font load

--- APP(FE)/App.tsx
+++ APP(FE)/App.tsx
@@ -1,17 +1,18 @@
 import MultiProvider from '@components/MultiProvider'
+import { TAPASquareIcon } from '@components/TPIcon'
+import { css } from '@emotion/native'
 import useAuth from '@hooks/auth'
 import { RootStackParamList } from '@navigators/RootStack'
 import RootStackNavigator from '@navigators/RootStackNavigator'
 import { NavigationContainer } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen'
 import { Provider as JotaiProvider } from 'jotai'
 import { useEffect } from 'react'
+import { View } from 'react-native'
 import { setCustomText, setCustomTextInput } from 'react-native-global-props'
 
 import 'react-native-gesture-handler'
 
-SplashScreen.preventAutoHideAsync()
 const App: React.FC = () => {
 	const firebaseUser = useAuth()
 
@@ -21,10 +22,7 @@ const App: React.FC = () => {
 	})
 
 	useEffect(() => {
-		if (!fontsLoaded) {
-			SplashScreen.preventAutoHideAsync()
-		} else {
-			SplashScreen.hideAsync()
+		if (fontsLoaded) {
 			setCustomText({
 				style: {
 					fontFamily: 'Pretendard',
@@ -37,6 +35,20 @@ const App: React.FC = () => {
 			})
 		}
 	}, [fontsLoaded])
+
+	if (!fontsLoaded) {
+		return (
+			<View
+				style={css`
+					flex: 1;
+					align-items: center;
+					justify-content: center;
+				`}
+			>
+				<TAPASquareIcon />
+			</View>
+		)
+	}
 
 	return (
 		<MultiProvider providers={[<JotaiProvider />]}>
