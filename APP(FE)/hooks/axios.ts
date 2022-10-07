b3976730a24bbbd44ccc4e11cab69951b0c8e03b
OXcarXierra <littlestein@snu.ac.kr>
2022-10-07 23:17:05 +0900

    feat: axios request

--- APP(FE)/hooks/axios.ts
+++ APP(FE)/hooks/axios.ts
@@ -10,17 +10,33 @@ const useAxios = () => {
 		const instance = axios.create({
 			baseURL: extra.backendBaseURL,
 		})
-		instance.interceptors.request.use(async config => {
-			const token = await user?.getIdToken()
-			if (!token) return config
-			return {
-				...config,
-				headers: {
-					...(config.headers ?? {}),
-					Authortization: `Bearer ${token}`,
-				},
-			}
-		})
+		instance.interceptors.request.use(
+			async config => {
+				const token = await user?.getIdToken()
+				if (!token) return config
+				return {
+					...config,
+					headers: {
+						...(config.headers ?? {}),
+						Authortization: `Bearer ${token}`,
+					},
+				}
+			},
+			error => {
+				console.error(error)
+				return Promise.reject(error)
+			},
+		)
+		instance.interceptors.response.use(
+			response => {
+				const res = response.data
+				return res
+			},
+			error => {
+				console.error(error)
+				return Promise.reject(error)
+			},
+		)
 		return instance
 	}, [user])
 	return axiosInstance
--- APP(FE)/store/atoms/community.ts
+++ APP(FE)/store/atoms/community.ts
@@ -5,3 +5,13 @@ import { PostType } from '@/types/community'
 export const postListAtom = atom<PostType[]>([])
 
 export const currentPostAtom = atom<PostType | null>(null)
+
+// export const getPostListAtom = atom(null, (_get, set) => {
+// 	set(
+// 		postListAtom,
+// 		request({
+// 			method: 'get',
+// 			url: '/community/post/',
+// 		}).data,
+// 	)
+// })
