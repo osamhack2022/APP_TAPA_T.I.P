a16829da86c9fc8f008e18a0ee3795d7766a02bd
Eunsoo Shin <62971511+esinx@users.noreply.github.com>
2022-10-09 06:32:37 +0000

    feat(backend-integration): tie user info with firebase credentials

--- APP(FE)/hooks/axios.ts
+++ APP(FE)/hooks/axios.ts
@@ -1,11 +1,11 @@
+import { userAtom } from '@store/atoms'
 import extra from '@utils/extra'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+import { useAtomValue } from 'jotai'
 import { useMemo } from 'react'
 
-import useAuth from './auth'
-
 const useAxios = () => {
-	const user = useAuth()
+	const user = useAtomValue(userAtom)
 	const axiosInstance = useMemo(() => {
 		const instance = axios.create({
 			baseURL: extra.backendBaseURL,
@@ -20,6 +20,16 @@ const useAxios = () => {
 					Authortization: `Bearer ${token}`,
 				},
 			}
+		})
+		instance.interceptors.response.use(undefined, async value => {
+			if (value instanceof AxiosError) {
+				console.error(
+					`AxiosError(${value.response?.status}/${value.code}): ${value.message}\n${value.response?.data}`,
+				)
+			} else {
+				console.error(value)
+			}
+			return value
 		})
 		return instance
 	}, [user])
