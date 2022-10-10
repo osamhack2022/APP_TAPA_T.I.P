61c3633f52fb4798fdc11cff393a547f4d34a407
GuHong Min <placidmoon1@gmail.com>
2022-10-10 14:07:04 +0900

    feat: get user info functioning

--- APP(BE)/app.py
+++ APP(BE)/app.py
@@ -1,9 +1,12 @@
-
 from flask import Flask, render_template
+from flask_cors import CORS
 import os
 import time, datetime
+from mySecrets import app_secret_key
 
 app = Flask(__name__)
+app.secret_key = app_secret_key
+cors = CORS(app)
 
 def format_server_time():
   server_time = time.localtime()
@@ -17,6 +20,9 @@ def index():
                 ]
 
   return render_template('index.html', times=dummy_times)
+
+import users
+app.register_blueprint(users.bp)
 
 if __name__ == '__main__':
   app.run(debug=True,host='127.0.0.1',port=int(os.environ.get('PORT', 8080)))
