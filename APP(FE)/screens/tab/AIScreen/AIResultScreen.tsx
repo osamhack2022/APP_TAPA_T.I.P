aee7ca7034d4112f5f74f5f2b31b3e70a52fb825
mscwrd02 <ehdtodvodl@gmail.com>
2022-10-25 00:25:40 +0000

    counselor screen complete

--- APP(FE)/screens/tab/AIScreen/AIResultScreen.tsx
+++ APP(FE)/screens/tab/AIScreen/AIResultScreen.tsx
@@ -1,11 +1,11 @@
 import FocusAwareStatusBar from '@components/FocusAwareStatusBar'
 import { css } from '@emotion/native'
+import useAxios from '@hooks/axios'
 import { RouteProp,useNavigation,useRoute } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { Text, View } from 'react-native'
 
 import { AINaviParamList } from './AINavigator'
-
 type NavigationProp = StackNavigationProp<
 	AINaviParamList,
 	'AIResult'	
@@ -17,16 +17,22 @@ type AIResultRouteProp = RouteProp<
 const AIResultScreen: React.FC = () => {
 	const navigation = useNavigation<NavigationProp>()
 	const {params : {answer},} =useRoute<AIResultRouteProp>()
+	const axios = useAxios()
+   
 	return (
 		<>
 			<View
 				style={css`
 					flex: 1;
 					align-items: center;
-					justify-content: center;
 				`}
 			>
-				<Text>AIResultScreen</Text>
+				<View>
+					<View>
+						<Text>AI 상담 결과</Text>
+						<Text>{}님의 상황은...</Text>
+					</View>
+				</View>
 				
 				
 					
---
+++ APP(FE)/types/user.ts
@@ -0,0 +1,12 @@
+export type UserType = {
+	id: string
+	username: string
+	email: string
+	name: string
+	service_number: string
+	rank: string
+	position: string
+	affiliated_unit: string
+	enlisted_at: number
+	discharged_at: number
+}
